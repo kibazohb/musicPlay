0343ff41e5c3f00ccc5623e85e63a087577122d7
kibazohb <kibazohb@mcmaster.ca>
2021-04-28 00:25:00 -0400

    create db & mongoose connection

---
+++ src/config/db.ts
@@ -0,0 +1,22 @@
+import mongoose, { Mongoose } from "mongoose";
+
+class db {
+
+  public async createConnection(){
+    try {
+      let mongooseUrl = process.env.MONGO_URI;
+      if (!mongooseUrl) {
+        console.error("The mongoURI was not set in the env variables");
+        process.exit(1);
+      }
+      await mongoose.connect(mongooseUrl);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+      
+    }
+  }
+}
+
+const connectDB = new db();
+export { connectDB as db };
