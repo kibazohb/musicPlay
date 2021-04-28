2275338fe61ebcf1c0dbddd0c1cf4c61a64bd187
kibazohb <kibazohb@mcmaster.ca>
2021-04-28 01:44:50 -0400

    db conn successfull

--- src/app.js
+++ src/app.js
@@ -7,12 +7,13 @@ const morgan = require('morgan')
 const path = require("path");
 const createError = require('http-errors')
 const fs = require("fs");
-const JavaScriptObfuscator = require("javascript-obfuscator");
+//const JavaScriptObfuscator = require("javascript-obfuscator");
 
 //start database connection
-import { connectDB } from "config/db.ts"
-
-const db = connectDB.createConnection();
+import { db } from './config/db';
+const startDbConnection = async () => db.createConnection();
+startDbConnection();
+console.log("************database connected****************");
 
 const app = express()
 
@@ -20,31 +21,31 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-});
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname + "/public/index.html"));
+// });
 
-// Send Style, do not change
-app.get("/style.css", function(req, res) {
-    //Feel free to change the contents of style.css to prettify your Web app
-    res.sendFile(path.join(__dirname + "/public/style.css"));
-});
+// // Send Style, do not change
+// app.get("/style.css", function(req, res) {
+//     //Feel free to change the contents of style.css to prettify your Web app
+//     res.sendFile(path.join(__dirname + "/public/style.css"));
+// });
 
 // Send obfuscated JS, do not change
-app.get("/index.js", function(req, res) {
-    fs.readFile(
-        path.join(__dirname + "/public/index.js"),
-        "utf8",
-        function(err, contents) {
-            const minimizedContents = JavaScriptObfuscator.obfuscate(contents, {
-                compact: true,
-                controlFlowFlattening: true,
-            });
-            res.contentType("application/javascript");
-            res.send(minimizedContents._obfuscatedCode);
-        }
-    );
-});
+// app.get("/index.js", function(req, res) {
+//     fs.readFile(
+//         path.join(__dirname + "/public/index.js"),
+//         "utf8",
+//         function(err, contents) {
+//             const minimizedContents = JavaScriptObfuscator.obfuscate(contents, {
+//                 compact: true,
+//                 controlFlowFlattening: true,
+//             });
+//             res.contentType("application/javascript");
+//             res.send(minimizedContents._obfuscatedCode);
+//         }
+//     );
+// });
 
 
 app.use(async(req, res, next) => {
